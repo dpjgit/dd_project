50ee8a75cd483b545428ab5954eb56b3fcbea672
Deepak P Jose <deepakpj88@gmail.com>
2022-11-09 14:56:26 +0530

    First commit

---
+++ new_test.py
@@ -0,0 +1,71 @@
+
+
+# Arithemetic operators
+#Assignment operators
+#Comparison operators
+#Logical operators
+#Identity operators
+#Membership operators
+# Bitwise operators
+
+# Arithemetic operators
+x = 10
+y = 4
+
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
+print(x % y)
+print(x ** y)
+print(x // y)
+
+# assignment operator
+
+x = 10
+x += 1
+print(x)
+x -= 1
+x *= 1
+x /= 1
+
+# comparison operator
+
+x = 10
+y = 9
+
+print(x == y)
+print(x != y)
+print(x < y)
+print(x > y)
+print(x <= y)
+print(x >= y)
+
+#Logical operator
+
+x = 2
+y = 10
+
+print(x == y and x < y)
+print(x == y or x < y)
+
+# identity operator
+
+x = ["That", "so"]
+y = ["That", "so"]
+
+print(x is y)
+print(x is not y)
+print(x == y)
+
+# Membership operator
+x = ["That", "so"]
+y = ["That", "so"]
+
+
+print("That" in x )
+# print("That" not in x )
+
+
+
+
